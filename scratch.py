9ff51e9717c3102d475d0d68ea3b42b966ebfd93
Elahrai <yagoo@yagooinc.com>
2020-02-20 14:07:51 +0900

    update

--- scratch.py
+++ scratch.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 
-def generator(i, j, k):
-    p = np.zeros((i, j, k))
-    for x in range(i):
+def function(m, ii, jj, kk):
+    epsn = 1e5
+    eps = 1e-5
+    n = 0
+    v = np.zeros([2000, 12])
+
+    p = np.zeros((ii, jj, kk))
+    for x in range(ii):
         counter = 0
-        for z in range(k):
+        for z in range(kk):
             total = 1
             left = False
             right = False
@@ -31,34 +36,20 @@ def generator(i, j, k):
                 p[x][2][z] = 1
             if down:
                 p[x][3][z] = 1
-        for a in range(j):
-            for b in range(k):
+        for a in range(jj):
+            for b in range(kk):
                 for item in np.nditer(p[x][a][b]):
                     if item == 1:
-                        p[x][a][b] = total/counter
-
-
-def function(m):
-    epsn = 1e5
-    eps = 1e-5
-    n = 0
-    v = np.zeros([20, 4])
-
-    p = [[0, 0.6, 0.4, 0],
-         [0, 0, 0, 1],
-         [0.3, 0, 0, 0.7]]
-
-    c = [[0, 5, 2, 0],
-         [0, 0, 0, 1],
-         [3, 0, 0, 4]]
+                        p[x][a][b] = total / counter
+    c = np.ones((ii, jj, kk))
 
     while epsn > eps:
         n += 1
-
         for i in range(m-1):
             v[n][i] = 0
-            for j in range(m):
-                v[n][i] = v[n][i] + p[i][j]*(c[i][j] + v[n-1][j])
+            for k in range(4):
+                for j in range(m):
+                    v[n][i] = v[n][i] + p[i][k][j]*(c[i][k][j] + v[n-1][j])
             e = 0
             for k in range(m):
                 e = e + abs(v[n][k] - v[n-1][k])
@@ -67,6 +58,6 @@ def function(m):
     print(v)
 
 
-function(4)
+function(12, 12, 4, 12)
 
 
